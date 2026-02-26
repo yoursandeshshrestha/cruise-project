2bf36372fa0f2db39f46ebcdda92a99d733c4e32
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2026-02-27 00:48:03 +0530

    fix: use PNG logo images instead of SVG

--- components/client/BrandLogo/index.tsx
+++ components/client/BrandLogo/index.tsx
@@ -6,48 +6,14 @@ interface BrandLogoProps {
 }
 
 export const BrandLogo: React.FC<BrandLogoProps> = ({ variant = 'color', className = '' }) => {
-  const isWhite = variant === 'white';
-  const primary = '#00A9FE';
-  const dark = '#001848';
-  const white = '#FFFFFF';
+  const src =
+    variant === 'white'
+      ? '/logos/simplecruise-logo-white.png'
+      : '/logos/simplecruise-logo-colour.png';
 
   return (
-    <div className={`flex items-center gap-3 ${className}`}>
-      <svg width="64" height="42" viewBox="0 0 120 80" fill="none" xmlns="http://www.w3.org/2000/svg" className="shrink-0" aria-label="Simple Cruise Parking Logo">
-        {/* Superstructure Stripes */}
-        <path 
-          d="M35 25 C 50 15, 75 12, 95 22" 
-          stroke={isWhite ? white : dark} 
-          strokeWidth="5" 
-          strokeLinecap="round"
-        />
-        <path 
-          d="M30 35 C 45 25, 70 22, 90 32" 
-          stroke={isWhite ? white : dark} 
-          strokeWidth="5" 
-          strokeLinecap="round"
-        />
-        
-        {/* Hull */}
-        <path 
-          d="M15 50 C 35 42, 70 35, 110 30 L 100 55 C 75 62, 45 62, 20 55 Z" 
-          fill={isWhite ? white : dark} 
-        />
-
-        {/* Waves */}
-        <path 
-          d="M20 65 C 40 70, 70 65, 95 60" 
-          stroke={isWhite ? white : primary} 
-          strokeWidth="4" 
-          strokeLinecap="round"
-        />
-        <path 
-          d="M30 74 C 50 79, 80 74, 90 70" 
-          stroke={isWhite ? white : primary} 
-          strokeWidth="4" 
-          strokeLinecap="round"
-        />
-      </svg>
+    <div className={`flex items-center ${className}`}>
+      <img src={src} alt="Simple Cruise logo" className="h-[50px] w-auto cursor-pointer" />
     </div>
   );
 };
